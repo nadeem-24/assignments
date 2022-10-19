948a85e723459c17d1af99dbb58ff9a7dcee1ed7
nadeem-24 <nknadeem800@gmail.com>
2022-10-19 17:24:26 +0530

    Final completion

---
+++ AiprusClone/app.js
@@ -0,0 +1,22 @@
+// const dropDownOne = document.querySelector('.dd-one');
+// const dropDownTwo = document.querySelector('.dd-Two');
+
+// const dropDownCompany = document.querySelector('.dropdown-one');
+
+// dropDownOne.addEventListener('mouseenter', function (){
+//     dropDownCompany.style.display = 'block';
+// });
+
+
+// dropDownOne.addEventListener('mouseleave', function (){
+//     dropDownCompany.style.display = 'none';
+// })
+
+// dropDownCompany.addEventListener('mouseenter', function (){
+//     dropDownCompany.style.display = 'block';
+// })
+
+
+// dropDownCompany.addEventListener('mouseleave', function (){
+//     dropDownCompany.style.display = 'none';
+// })
